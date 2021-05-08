6a85f280f2a85ab4d3d9d58cff47ccfeeb70d0c5
vidongls <vidongls12345@gmail.com>
2021-05-08 22:01:32 +0700

    ktra

--- ktra_l1/b3_.cpp
+++ ktra_l1/b3_.cpp
@@ -1,15 +1,15 @@
-﻿#include <iostream>
+﻿ï»¿#include <iostream>
 #include<string>
 #include<fstream>
 using namespace std;
-//---------- Định nghĩa ---------------
 
 struct sinh_vien
 {
 	string hoten;
 	string gioitinh;
-	int tuoi;
-	float dtb;
+	string ngaysinh;
+	int msv;
+	float mac, daiso, cpp;		
 };
 typedef struct sinh_vien sv;
 //-------------------------
@@ -17,9 +17,12 @@ void nhap_ds_sv(sv ds_sv[], int n);
 //----------------------
 void xuat_ds_sv(sv ds_sv[], int n);
 //----------------------
-void sapxep_dtb(sv ds_sv[], int n);
+void xoa(sv ds_sv[], int& n, int vt);
 //------------------------
-void chuan_hoa(string& str);
+void nhapdiem(sv& s);
+void them_diem(sv ds_sv[], int msv, int n);
+void xuat_diem(sv s);
+void xuat_diem_ds(sv ds_sv[], int n);
 //=====-----------------------
 istream& operator >> (istream& is, sv& s);
 ostream& operator << (ostream& os, sv s);
@@ -30,28 +33,29 @@ int main()
 	while (check)
 	{
 		system("cls");
-		cout << "1. Them sinh vien"<<endl;
+		cout << "1. Them sinh vien" << endl;
 		cout << "2. Xuat danh sach sinh vien" << endl;
-		cout << "3. Sap xep sinh vien theo dtb" << endl;
-		cout << "4. Thoat" << endl;
+		cout << "3. Xoa" << endl;
+		cout << "4. Nhap diem" << endl;
+		cout << "5. Thoat" << endl;
 		int lc;
 		cout << "Moi nhap lua chon: "; cin >> lc;
 		switch (lc)
 		{
 		case 1: {
 			cout << "-------------- Nhap SV --------------" << endl;
-			
 			int sl;
 			do
 			{
 				cout << "Moi nhap so luong sinh vien :"; cin >> sl;
-				if (sl <=0)
+				if (sl <= 0)
 				{
 					cout << "So luong sv khong hop le!" << endl;
 				}
-			} while (sl <=0);
+			} while (sl <= 0);
 			for (int i = 0; i < sl; i++)
-			{	cin.ignore();
+			{
+				cin.ignore();
 				sv s;
 				cout << "\t\t----------- SV " << i << " ---------" << endl;
 				cin >> s;
@@ -69,99 +73,107 @@ int main()
 		}
 		case 3: {
 			if (n == 0)break;
-			cout << "\t\t--------- Danh sach sau khi sap xep ---------"<<endl;
-			sapxep_dtb(ds_sinhvien, n);
+			int vt = 0;
+			cout << "Moi nhap vi tri can xoa: "; cin >> vt;
+			cout << "\t\t--------- Danh sach xoa ---------" << endl;
+			xoa(ds_sinhvien, n, vt);
 			xuat_ds_sv(ds_sinhvien, n);
 			system("pause");
 			break;
 		}
 		case 4: {
+			int msv;
+			cout << "Nhap MSV: "<<endl; cin >> msv;
+			them_diem(ds_sinhvien, msv, n);	
+			xuat_ds_sv(ds_sinhvien, n);
+			xuat_diem_ds(ds_sinhvien,n);
+			system("pause");
+			break;
+		}
+		case 5: {
 			int l;
-			cout << "Ban co thuc su muon thoat ?"<<endl;
-			cout << "1. Yes\t\t2. No"<<endl;	 cin >> l;
-			if (l == 1)check = false;			  
+			cout << "Ban co thuc su muon thoat ?" << endl;
+			cout << "1. Yes\t\t2. No" << endl;	 cin >> l;
+			if (l == 1)check = false;
 			break;
 		}
 		}
 	}
 }
 istream& operator >> (istream& is, sv& s) {
+	cout << "Nhap MSV: "; cin >> s.msv;
+	cin.ignore();
 	cout << "Nhap ho ten: "; getline(cin, s.hoten);
 	cout << "Nhap gioi tinh: "; getline(cin, s.gioitinh);
-	cout << "Nhap tuoi: "; cin >> s.tuoi;
-	cout << "Nhap diem: "; cin >> s.dtb;
+	cout << "Nhap ngay sinh: "; getline(cin, s.ngaysinh);
 	return is;
 }
-void nhap_ds_sv(sv ds_sv[], int n) {		
+void nhap_ds_sv(sv ds_sv[], int n) {
 	for (int i = 0; i < n; i++)
 	{
 		cin.ignore();
 		cin >> ds_sv[i];
 	}
 }
+
 ostream& operator << (ostream& os, sv s) {
-	chuan_hoa(s.hoten);
-	cout << "Ho ten: " <<  s.hoten << endl;
+	cout << "MSV: " << s.msv << endl;
+	cout << "Ho ten: " << s.hoten << endl;
 	cout << "Gioi tinh: " << s.gioitinh << endl;
-	cout << "Tuoi: " << s.tuoi << endl;
-	cout << "Diem tb: " << s.dtb << endl;
+	cout << "Ngay sinh: " << s.ngaysinh << endl;
 	return os;
 }
+
 void xuat_ds_sv(sv ds_sv[], int n) {
-	
+
 	for (int i = 0; i < n; i++)
 	{
 		cout << "\t\t----------- SV " << i << " ---------" << endl;
 		cout << ds_sv[i];
 	}
 }
-void sapxep_dtb(sv ds_sv[],int n){
-	for (int i = 0; i < n-1; i++)
-	{
-		for (int j = 1; j < n; j++)
-		{
-			if (ds_sv[i].dtb > ds_sv[j].dtb)
-			{
-				swap(ds_sv[i], ds_sv[j]);
-			}
-		}
-	}
-}	
-void chuan_hoa(string& str)
+void xoa(sv ds_sv[], int& n, int vt)
 {
-	while (str[0] == ' ')
+	for (int i = vt; i < n - 1; i++)
 	{
-		str.erase(str.begin() + 0);
+		ds_sv[i] = ds_sv[i + 1];
 	}
-	while (str[str.length() - 1] == ' ')
-	{
-		str.erase(str.begin() + (str.length() - 1));
-	}
-	for (int i = 0; i < str.length(); i++)
-	{
-		if (str[i] == ' ' && str[i + 1] == ' ')
-		{
-			str.erase(str.begin() + i);
-			i--;
-		}
-	}
-	for (int i = 0; i < str.length(); i++)
-	{
-		if (str[i] >= 65 && str[i] <= 90)
-		{
-			str[i] += 32;
-		}
-	}
-	if (str[0] >= 97 && str[0] <= 122) str[0] -= 32;
-	for (int i = 1; i < str.length() - 1; i++)
-	{
+	n--;
+}
 
-		if (str[i] == ' ' && str[i + 1] != ' ')
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] -= 32;
-			}
+void nhapdiem(sv& s)
+{
+	cout << "Nhap diem Mac-Lenin :"; cin >> s.mac;
+	cout << "Nhap diem Dai so: "; cin >> s.daiso;
+	cout << "Nhap diem C++:"; cin >> s.cpp;
+}
+
+void them_diem(sv ds_sv[], int msv, int n) {
+	bool found = false;
+	for (int i = 0; i < n; i++) {
+		if (ds_sv[i].msv == msv) {
+			found = true;
+			cout << "\nThem sinh vien co MSV = " << msv <<endl;
+			nhapdiem(ds_sv[i]);
+			break;
 		}
+	}
+	if (found == false) {
+		cout << "Sinh vien khong ton tai !"<<endl;
+	}
+}
+void xuat_diem(sv s)
+{
+	cout << "Dai so " <<s.daiso << endl;
+	cout << "Mac-Lenin " << s.mac << endl;
+	cout << "C++" << s.cpp << endl;
+}
+void xuat_diem_ds(sv ds_sv[], int n)
+{
+	cout << "\t\t======== XUAT =========" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\t\t----------- SV " << i << " ---------" << endl;
+		xuat_diem(ds_sv[i]);
 	}
 }
